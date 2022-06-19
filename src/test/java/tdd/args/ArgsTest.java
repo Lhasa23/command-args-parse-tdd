584177051bcdb128ae1d1e35ce179224cf2c62bf
eleven <sydryy@gmail.com>
2022-06-19 13:33:34 +0800

    args happy path done

--- src/test/java/tdd/args/ArgsTest.java
+++ src/test/java/tdd/args/ArgsTest.java
@@ -9,8 +9,6 @@ public class ArgsTest {
     // -l -p 8080 -d /usr/logs
     // index: [-l], [-p, 8080], [-d, /usr/logs]
     // Map: {-l:[], -p:[8080], -d:[.usr/logs]}
-    // happy path:
-    // single test:
     @Test
     public void should_set_boolean_option_to_true_if_l_flag_present() {
         BooleanOption option = Args.parse(BooleanOption.class, "-l");
@@ -43,8 +41,19 @@ public class ArgsTest {
 
     static record StringOption(@Option("d") String directory) {
     }
-    // multi test:
-    // TODO: -l -p 8080 -d /usr/logs => logging: true, port: 8080, directory: /usr/logs
+
+    @Test
+    public void should_parse_multi_args() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+
+
     // sad path:
     // TODO: -l t / -l t f => error
     // TODO: -p / -p 8080 8081 => error
@@ -54,17 +63,7 @@ public class ArgsTest {
     // TODO: -p => 0
     // TODO: -d => ""
 
-    @Test
-    @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-        assertEquals("/usr/logs", options.directory());
-    }
 
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-    }
 
     // -g this is a list -d 1 2 -3 5
     @Test
