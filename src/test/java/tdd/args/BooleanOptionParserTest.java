de94280ee97f72a147fda839831df7fbfd451172
eleven <sydryy@gmail.com>
2022-07-03 00:11:53 +0800

    complete test refactor

--- src/test/java/tdd/args/BooleanOptionParserTest.java
+++ src/test/java/tdd/args/BooleanOptionParserTest.java
@@ -8,13 +8,19 @@ import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BooleanOptionParserTest {
-    @Test
+
+    @Test // happy path
+    public void should_true_if_boolean_option_present() {
+        assertTrue(new BooleanOptionParser().parse(asList("-l"), option("l")));
+    }
+
+    @Test // sad path
     public void should_not_accept_extra_argument_for_boolean_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> new BooleanOptionParser().parse(asList("-l", "t"), option("l")));
         assertEquals("l", e.getOption());
     }
 
-    @Test
+    @Test // default value
     public void should_false_if_boolean_option_is_empty() {
         assertFalse(new BooleanOptionParser().parse(asList(), option("l")));
     }
--- src/test/java/tdd/args/SingleValuedOptionParserTest.java
+++ src/test/java/tdd/args/SingleValuedOptionParserTest.java
@@ -12,7 +12,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import static tdd.args.BooleanOptionParserTest.option;
 
 public class SingleValuedOptionParserTest {
-    @Test
+    @Test // p happy path
+    public void should_set_8080_for_port_option_argument() {
+        Object parsed = new Object();
+        Function<String, Object> parse = (it) -> parsed;
+        Object whatever = new Object();
+        assertSame(parsed,
+                new SingleValuedOptionParser<>(whatever, parse).parse(asList("-p", "8080"), option("p")));
+    }
+
+    @Test // d happy path
+    public void should_set_dir_for_directory_option_argument() {
+        Object parsed = new Object();
+        Function<String, Object> parse = (it) -> parsed;
+        Object whatever = new Object();
+        assertSame(parsed,
+                new SingleValuedOptionParser<>(whatever, parse).parse(asList("-d", "/usr/logs"), option("d")));
+    }
+
+    @Test // p sad path
     public void should_not_accept_extra_argument_for_port_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
             new SingleValuedOptionParser<>(0, Integer::parseInt).parse(asList("-p", "8080", "8081"), option("p"));
@@ -21,7 +39,7 @@ public class SingleValuedOptionParserTest {
         assertEquals("p", e.getOption());
     }
 
-    @Test
+    @Test // d sad path
     public void should_not_accept_extra_argument_for_directory_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
             new SingleValuedOptionParser<>("", Integer::parseInt).parse(asList("-d", "/usr/ssh", "/usr/logs"), option("d"));
@@ -30,7 +48,7 @@ public class SingleValuedOptionParserTest {
         assertEquals("d", e.getOption());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest // p sad path
     @ValueSource(strings = {"-p -d", "-p"})
     public void should_not_accept_insufficient_argument_for_port_option(String arguments) {
         InsufficientArgumentException e = assertThrows(InsufficientArgumentException.class, () -> {
@@ -40,7 +58,7 @@ public class SingleValuedOptionParserTest {
         assertEquals("p", e.getOption());
     }
 
-    @ParameterizedTest
+    @ParameterizedTest // d sad path
     @ValueSource(strings = {"-d", "-d -p"})
     public void should_not_accept_insufficient_argument_for_directory_option(String arguments) {
         InsufficientArgumentException e = assertThrows(InsufficientArgumentException.class, () -> {
@@ -50,15 +68,11 @@ public class SingleValuedOptionParserTest {
         assertEquals("d", e.getOption());
     }
 
-    @Test
+    @Test // p default value
     public void should_set_default_value_0_for_port_option() {
-        assertEquals(0,
-                new SingleValuedOptionParser<>(0, Integer::parseInt).parse(asList(), option("p")));
-    }
-
-    @Test
-    public void should_set_default_value_empty_for_directory_option() {
-        assertEquals("",
-                new SingleValuedOptionParser<>("", String::valueOf).parse(asList(), option("d")));
+        Function<String, Object> whatever = (it) -> null;
+        Object defaultValue = new Object();
+        assertSame(defaultValue,
+                new SingleValuedOptionParser<>(defaultValue, whatever).parse(asList(), option("p")));
     }
 }
